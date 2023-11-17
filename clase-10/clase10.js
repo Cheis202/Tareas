da2101859dbae0906b0d5d5608796900592c5157
LMT <Cheis0.0Brock@gmail.com>
2023-11-17 09:08:23 -0300

    correcciones clase 10

--- clase-10/clase10.js
+++ clase-10/clase10.js
@@ -6,24 +6,26 @@ const $btnJugar = document.getElementById("btn-iniciar")
 let secuenciaMaquina = []
 let secuenciaUsuario = []
 let ronda = 0
-const delayJugador = (secuenciaMaquina.length + 2) * 1000
+let juegoTerminado = false
 
-const resaltar = (color) =>{
+const resaltarColor = (color) =>{
   color.classList.add("resaltar");
   setTimeout(()=>{
     color.classList.remove("resaltar")
-  },500)
+  },1000)
 }
 
 const resaltarColorUsuario = (e)=>{
   const colorSeleccionado = e.target
   const indiceColorSeleccionado = $colores.indexOf(e.target);
   secuenciaUsuario.push(indiceColorSeleccionado)
-  comprobar()
-  resaltar(colorSeleccionado)
+  if(secuenciaUsuario.length === secuenciaMaquina.length){
+    comprobarSecuencia()
+  }
+  resaltarColor(colorSeleccionado)
 }
 
-const indiceAleatorio = ()=>{
+const generarColorAleatorio = ()=>{
   return Math.floor(Math.random() * 4);
 }
 
@@ -36,12 +38,12 @@ const deshabilitarUsuario = ()=>{
   $colores.forEach(()=>{})
 }
 
-const estadoJuego = (texto,)=>{
+const actualizarEstado = (texto)=>{
   $estado.textContent = texto
 
 }
 
-const siguienteRonda = ()=>{
+const actualizarRonda = ()=>{
   ronda++
   $ronda.textContent = `Ronda ${ronda}`
 }
@@ -51,21 +53,20 @@ const reiniciarJuego = ()=>{
   secuenciaUsuario = []
   ronda = 0
 }
-const perdiste=()=>{
+const perder=()=>{
   deshabilitarUsuario()
-  estadoJuego("Perdiste, volve a empezar")
+  actualizarEstado("Perdiste, volve a empezar")
 }
 const turnoMaquina = ()=>{
-  estadoJuego("Turno de la Maquina")
-  siguienteRonda()
+  actualizarEstado("Turno de la Maquina")
+  actualizarRonda()
   deshabilitarUsuario()
-  
-  
-  secuenciaMaquina.push(indiceAleatorio())
+  const delayJugador = (secuenciaMaquina.length + 2) * 1000
+  secuenciaMaquina.push(generarColorAleatorio())
   secuenciaMaquina.forEach((color,index)=>{
     const delayMaquina = (index + 1) * 1000
     setTimeout(()=>{
-      resaltar($colores[color])
+      resaltarColor($colores[color])
     },delayMaquina)
   })
 
@@ -73,21 +74,24 @@ const turnoMaquina = ()=>{
     turnoJugador()
   },delayJugador)
 }
-const comprobar= ()=>{
+const comprobarSecuencia= ()=>{
+  const delayJugador = (secuenciaMaquina.length + 1) * 1000
   secuenciaMaquina.forEach((color, index)=>{
     if(color !== secuenciaUsuario[index]){
-      perdiste()
+      perder()
+      juegoTerminado = true
       return
     }
   })
-  setTimeout(()=>{
-    secuenciaUsuario = []
-    turnoMaquina()
-  },delayJugador + 1000)
-  
+  if (!juegoTerminado) {
+    setTimeout(() => {
+      secuenciaUsuario = [];
+      turnoMaquina();
+    }, delayJugador + 1000);
+  }
 }
 const turnoJugador = ()=>{
-  estadoJuego("Turno del Jugador")
+  actualizarEstado("Turno del Jugador")
   habilitarUsuario()
 }
 $btnJugar.addEventListener("click",turnoMaquina)
